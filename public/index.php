493898287e6bbd675a7e0a385ebc2a574124beab
Abdoul Aziz BALDE <59298812+baldazi@users.noreply.github.com>
2024-02-06 22:54:49 +0100

    index page

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
-<?php
-
-echo "hello world";
-
-phpInfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn Php | docker</title>
+</head>
+<body>
+    <?= "hello php" ?>
+</body>
+</html>
